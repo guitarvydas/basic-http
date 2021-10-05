64d802a0a6f9ec3f6ce5b32b3dddc4303e3dda48
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-05 08:12:55 -0400

    client6 modified for localhost:8000 and routes.js

--- client6.js
+++ client6.js
@@ -1,6 +1,7 @@
-const https = require ('https');
+const http = require ('http');
 
-let request = https.get ('https://jsonplaceholder.typicode.com/user?_limit=2', (res) => {
+//let request = http.get ('http://jsonplaceholder.typicode.com/user?_limit=2', (res) => {
+let request = http.get ('http://localhost:8000/authors', (res) => {
     if (res.statusCode !== 200) {
 	console.error (`Did not get an OK from the server. Code ${res.statusCode}`);
 	res.resume ();
