5e42ac89edc6c9ecacb7532d455ee8d9dc338b1b
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-05 23:02:28 -0400

    received data arrives after function sendReq returns

--- client6.js
+++ client6.js
@@ -3,29 +3,34 @@ const http = require ('http');
 const options = { method: 'POST'
 };
 
-//let request = http.get ('http://jsonplaceholder.typicode.com/user?_limit=2', (res) => {
-let request = http.request ('http://localhost:8000/authors',
-			    options,
-			    (res) => {
-				if (res.statusCode !== 201) {
-				    console.error (`Did not get an OK from the server. Code ${res.statusCode}`);
-				    res.resume ();
-				    return;
-				}
-				
-				let data = '';
-				res.on('data', (chunk) => { 
-				    data += chunk;
-				});
-				res.on('close', () => {
-				    console.log('Retrieved all data'); 
-				    console.log(JSON.parse(data));
-				});
-				
-				request.on('error', (err) => {
-				    console.error(`Encountered an error trying to make a request: ${err.message}`)});
-			    });
-const reqData = { message: "hello" };
-request.write (JSON.stringify (reqData));
-request.end ();
+var data = '';
 
+function sendReq () {
+    let request = http.request ('http://localhost:8000/authors',
+				options,
+				(res) => {
+				    if (res.statusCode !== 201) {
+					console.error (`Did not get an OK from the server. Code ${res.statusCode}`);
+					res.resume ();
+					return;
+				    }
+				
+				    res.on('data', (chunk) => { 
+					data += chunk;
+				    });
+				    res.on('close', () => {
+					console.log('Retrieved all data'); 
+					console.log(JSON.parse(data));
+				    });
+				    
+				    request.on('error', (err) => {
+					console.error(`Encountered an error trying to make a request: ${err.message}`)});
+				});
+    const reqData = { message: "hello" };
+    request.write (JSON.stringify (reqData));
+    request.end ();
+}
+
+sendReq ();
+
+console.log ("received: /" + data + "/");
